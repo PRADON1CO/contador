35e49253505494425bcfae222a181fe0c2bcc4df
Prado Nicolas <nicolas88prado@gmail.com>
2024-04-08 20:43:17 -0300

    agregamos el componente informacion.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-
   return (
-    <h1>Prueba de titulo</h1>
-  )
+    <>
+      <main className="container my-5">
+        <h1>Primer proyecto con React</h1>
+        
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
