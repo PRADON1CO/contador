bcc9f54a20c371e090450f5a14e1602b0afa0129
Prado Nicolas <nicolas88prado@gmail.com>
2024-04-08 21:24:00 -0300

    agregamos el maquetado del componente contador

---
+++ src/components/Contador.jsx
@@ -0,0 +1,12 @@
+const Contador = () => {
+    return (
+        <section className="text-center">
+            <h2>Contador</h2>
+        <button className="btn btn-primary me-2">+1</button>
+        <button className="btn btn-danger">-1</button>
+        </section>  
+        
+    );
+};
+
+export default Contador;
