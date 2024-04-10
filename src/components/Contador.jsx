f2b1c5c0fe18ee6073944d8e9a46206bbeca81ee
Prado Nicolas <nicolas88prado@gmail.com>
2024-04-10 20:38:46 -0300

    trabajamos con visualDom

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
+
 const Contador = () => {
     //aqui agrego la logica del componente
-    let numero = 10;
+    //let numero = 10;
+    //aqui me creo el state
+    const [numero, setNumero] = useState(0)
 
-    const sumar =()=> {
-        numero = numero +1
-        console.log(numero);
-    }
+    const sumar =()=> {setNumero(numero +1)}
+
 
     return (
         //aqui puedo agregar un poquito de logica
@@ -14,7 +16,7 @@ const Contador = () => {
             <h2>Contador</h2>
             <p>{numero}</p>
             <button className="btn btn-primary me-2" onClick={sumar}>+1</button>
-            <button className="btn btn-danger">-1</button>
+            <button className="btn btn-danger" onClick={restar}>-1</button>
         </section>  
         
     );
